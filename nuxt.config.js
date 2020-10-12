3558e0468b21607a3ffaabcdeb19c1f639f1b5d1
abdulbasit <mrspeed518@gmail.com>
2020-10-12 22:48:24 +0100

    adding env

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,52 +1,69 @@
+import { TRUE } from "node-sass";
+
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'ecng',
+    title: "ecng",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    "@assets/css/main"
-  ],
+  css: ["@assets/css/main"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    '@nuxtjs/fontawesome'
-  ],
+  buildModules: ["@nuxtjs/fontawesome"],
   fontawesome: {
-    component: 'fa',
+    component: "fa",
     icons: {
-      solid: true, brands: true
+      solid: true,
+      brands: true
     }
   },
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
+    "@nuxtjs/pwa",
     // '@nuxtjs/fontawesome'
+    "@nuxtjs/firebase"
+    // adding firebase
   ],
+  firebase: {
+    config: {
+      apiKey: "<apiKey>",
+      authDomain: "<authDomain>",
+      databaseURL: "<databaseURL>",
+      projectId: "<projectId>",
+      storageBucket: "<storageBucket>",
+      messagingSenderId: "<messagingSenderId>",
+      appId: "<appId>",
+      measurementId: "<measurementId>"
+    },
+    services: {
+      auth: true,
+      firestore: true,
+      storage: true,
+      messaging: true,
+      performance: true,
+      analytics: true
+    }
+  },
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
-}
+  build: {}
+};

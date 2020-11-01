709fe4ac41170d519ce4e45cff972e59ede3775b
abdulbasit <mrspeed518@gmail.com>
2020-11-01 06:57:55 +0100

    improved meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: "ecng",
+    title: "Ecng News",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
